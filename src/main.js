bddc0f11141d5fd8aa68bea911057dce5967c4ec
Oluwwatimilehin Awoniyi <awoniyitimi@outlook.com>
2022-06-23 04:07:23 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const menuBtn = document.querySelector(".menu");
+
+// menuBtn.addEventListener("click", () => {
+//   console.log("click");
+//   //   menuBtn.classList.replace(`fa fa-bars`, `fa-solid fa-x`);
+// });
+
+menuBtn.addEventListener("click", () => console.log("clik"));
+
+// console.log('gg');
