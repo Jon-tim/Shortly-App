1911df1226b71faaaa3660add826cf461af3d816
Oluwwatimilehin Awoniyi <awoniyitimi@outlook.com>
2022-06-25 18:25:47 +0100

    Updated Input fields

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 const menu = document.querySelector(".hamburger");
 const mobileNav = document.querySelector(".mobile-menu");
 
+const linkInput = document.querySelector(".link-input");
+const linkInputHolder = document.querySelector(".link-input").placeholder;
+const linkSubmit = document.querySelector(".link-submit");
+
 const bars = document.querySelectorAll("span");
 
 menu.addEventListener("click", () => {
@@ -10,13 +14,6 @@ menu.addEventListener("click", () => {
   mobileNav.classList.toggle("menu-closed");
 });
 
-// const menuBtn = document.querySelector(".menu");
-
-// // menuBtn.addEventListener("click", () => {
-// //   console.log("click");
-// //   //   menuBtn.classList.replace(`fa fa-bars`, `fa-solid fa-x`);
-// // });
-
-// menuBtn.addEventListener("click", () => console.log("clik"));
-
-// console.log('gg');
+linkSubmit.addEventListener("click", () => {
+  linkInput.classList.toggle("invalid");
+});
