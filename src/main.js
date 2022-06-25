55b29a55dbc7f6093767d098c6ca94acc149b186
Oluwwatimilehin Awoniyi <awoniyitimi@outlook.com>
2022-06-25 18:47:58 +0100

    Updated Error message

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ const menu = document.querySelector(".hamburger");
 const mobileNav = document.querySelector(".mobile-menu");
 
 const linkInput = document.querySelector(".link-input");
-const linkInputHolder = document.querySelector(".link-input").placeholder;
 const linkSubmit = document.querySelector(".link-submit");
+const errorMessage = document.querySelector(".error");
 
 const bars = document.querySelectorAll("span");
 
@@ -15,5 +15,13 @@ menu.addEventListener("click", () => {
 });
 
 linkSubmit.addEventListener("click", () => {
-  linkInput.classList.toggle("invalid");
+  if (linkInput.value === "") {
+    linkInput.classList.add("invalid");
+    linkSubmit.style.marginTop = "0.5rem";
+    errorMessage.style.display = "block";
+  } else {
+    linkInput.classList.remove("invalid");
+    linkSubmit.style.marginTop = "1rem";
+    errorMessage.style.display = "none";
+  }
 });
