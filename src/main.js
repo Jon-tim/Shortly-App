93ae90b126a9dbfd5e57d705dce0f56ac81d2a0a
Oluwwatimilehin Awoniyi <awoniyitimi@outlook.com>
2022-06-27 00:26:06 +0100

    Worked on the API

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ const linkInput = document.querySelector(".link-input");
 const linkSubmit = document.querySelector(".link-submit");
 const errorMessage = document.querySelector(".error");
 
+const shortenedLinkContainer = document.querySelector(".shortened-links");
+
+// const copyUrlBtn = document.querySelectorAll(".copyBtn");
+
 const bars = document.querySelectorAll("span");
 
 menu.addEventListener("click", () => {
@@ -14,7 +18,25 @@ menu.addEventListener("click", () => {
   mobileNav.classList.toggle("menu-closed");
 });
 
-linkSubmit.addEventListener("click", () => {
+const shortener = async () => {
+  const response = await fetch(
+    `https://api.shrtco.de/v2/shorten?url=${linkInput.value}`
+  );
+  const data = await response.json();
+
+  const htmlUrls = `<div class="cont">
+      <p class="cont-one">${data.result.original_link}</p>
+      <div class="cont-two">
+      <p>${data.result.full_short_link}</p>
+      <button class="copyBtn">copy</button>
+      </div>
+      </div>`;
+
+  shortenedLinkContainer.insertAdjacentHTML("afterbegin", htmlUrls);
+};
+
+linkSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
   if (linkInput.value === "") {
     linkInput.classList.add("invalid");
     linkSubmit.style.marginTop = "0.5rem";
@@ -23,13 +45,6 @@ linkSubmit.addEventListener("click", () => {
     linkInput.classList.remove("invalid");
     linkSubmit.style.marginTop = "1rem";
     errorMessage.style.display = "none";
+    shortener();
   }
 });
-
-
-
-
-//CREATE SHORTENED LINKS THROUGH DOM
-// function shortened () {
-//   ``
-// }
